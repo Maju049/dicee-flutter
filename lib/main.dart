786dfa7f7bb0d4a52ff432ee6618baffd6e4b6c5
Maju049 <64065807+Maju049@users.noreply.github.com>
2020-09-22 19:09:00 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,3 +21,65 @@ class DicePage extends StatelessWidget {
     return Container();
   }
 }
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent[100],
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.blue,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int dice01 = 1;
+  int dice02 = 1;
+
+  void changeDice() {
+    dice01 = Random().nextInt(6) + 1;
+    dice02 = Random().nextInt(6) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(
+                10.0,
+              ),
+              child: Image(
+                image: AssetImage('images/dice$dice02.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  changeDice();
+                });
+              },
+              child: Image.asset('images/dice$dice01.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
